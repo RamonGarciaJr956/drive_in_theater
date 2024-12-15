f1d1ab27bcdb74fde4c162e47f73249f5fbebe60
LoneCuriosity <ramon@ramongarciajr.tech>
2024-12-14 19:49:08 -0600

    fixed type issue

--- src/app/api/buy-ticket/route.ts
+++ src/app/api/buy-ticket/route.ts
@@ -104,7 +104,7 @@ export async function POST(request: NextRequest) {
                 showingId: body.showingId,
             },
             currency: "usd"
-        }, {});
+        });
 
         return NextResponse.json({ checkoutSessionUrl: checkoutSession.url }, { status: 200 });
     } catch (error) {
